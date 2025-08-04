42b899d6d5cced269cf7346085f40698d3d7acab
Srujana Tangudu <161055910+Srujanatangudu4@users.noreply.github.com>
2025-08-04 15:01:37 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,9 @@
 import java.util.*;
 public class Hello{
 	public static void main(String args[]){
-		System.out.println("Hello World Jenkins!......");
+		System.out.println("Hello World ");
 	}
 
 }
+
 
